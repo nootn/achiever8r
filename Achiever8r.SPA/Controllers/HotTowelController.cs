78f90bb2b5024ebe5d2fb9232391e68900fc31e9
nootn <me@nootn.com.au>
2013-06-01 19:27:19 +1000

    Adds blank SPA project

---
+++ Achiever8r.SPA/Controllers/HotTowelController.cs
@@ -0,0 +1,16 @@
+using System.Web.Mvc;
+
+namespace Achiever8r.SPA.Controllers
+{
+    public class HotTowelController : Controller
+    {
+        //
+        // GET: /HotTowel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
